9f8b0e242834d072c432ad1e6c28812ea28009aa
Ameth <38502560+anmeth191@users.noreply.github.com>
2021-04-12 21:17:32 -0400

    Add files via upload
    Updated the Style for the users post and also rendered the posts in a new component

--- DisplayPost.js
+++ DisplayPost.js
@@ -1,52 +1,56 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
+import HeaderComponent from '../Header';
 
 
 
 class DisplayPost extends React.Component{
-   state = {posts:[]  , user:null}
+ state = {posts:[] , post:''}
 
-    postsUser = async ()=>{
 
-        
-        const requestPost = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-        return requestPost;
-    }
 
-componentWillMount(){
-  
-    this.postsUser().then(response => { return this.setState({posts:response.data})}).catch(err =>{ return `You have error: ${err}`})
-    const { post } = this.props.match.params; 
-    this.setState({user:post} , ()=> {
-     return this.state.user;
-    });
-    
-    
-  
+
+
+componentDidMount(){
+    const idPost = this.props.match.params.post;
+    this.setState({post:idPost} , ()=>{return this.state.post})
+//set the post request reqeut to state posts
+    this.postRequest().then(response => { this.setState({posts:response.data} ,()=>{return this.state.posts})}).catch(err => {
+        return <h1>{err}</h1>
+    })
 }
+
+postRequest = async ()=>{
+
+    const postRequest = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    return postRequest;
+
+}
+
 
 render(){
-
-   
-    return(
-        <div>{console.log(this.state.user)}
-            {this.state.posts.map( post => {
-              if(post.userId == this.state.user) {
-                   return(
-                       <div>
-                       <h3>{post.title}</h3>
-                       <h5>{post.body}</h5>
-                       </div>
-                       
-                   )
-
-              }//end of the if
+    console.log(this.props.match.params.post)
+    return(  
         
-        })}</div>
-    )
-}
-
+        <div> <HeaderComponent /> 
+        {  
+            this.state.posts.map( element => {
+               
+             if(element.userId == this.props.match.params.post){  
+              return ([<div className="postContainer">
+                  <div className="postContainer-card">
+                  <div className="postContainer-card-title"><h1 className="postContainer-card-title-header">{element.title} </h1></div>
+                  <div className="postContainer-card-body"><span className="postContainer-card-body-span">{element.body}</span></div> 
+                 </div>
+                 </div> 
+              ])//end of the return
+            }//end of the if
+            })//end of the map
+            }
+            </div>
+    )//end of the return
+}//end of the render 
 
 }
 
